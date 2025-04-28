2363f97b1301c465fb62840ce30784b73bcc4c0f
abaskir <alper.baskir@rebtel.com>
2025-04-29 00:45:07 +0200

    fix for trip type selector component

--- components/TripTypeSelector.jsx
+++ components/TripTypeSelector.jsx
@@ -2,11 +2,11 @@
 
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const TripTypeSelector = ({ tripType, onChange }) => {
+const TripTypeSelector = ({ onChange, tripType = "oneWay" }) => {
   return (
     <div className="flex flex-col space-y-2">
       <label className="text-gray-700 font-medium">Trip Type</label>
-      <Tabs defaultValue={tripType} onValueChange={onChange} className="w-full">
+      <Tabs value={tripType} onValueChange={onChange} className="w-full">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="oneWay">One-way</TabsTrigger>
           <TabsTrigger value="roundTrip">Round Trip</TabsTrigger>
